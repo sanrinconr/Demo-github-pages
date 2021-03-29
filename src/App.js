0c59945a874bcfa931cb1bab145e4b12cda9cf4a
Santiago Rincon <santiagorincon2001@gmail.com>
2021-03-29 08:08:45 -0500

    Title changed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          ¡Finally in github pages!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
